ee2de507437f87fd5dc632b96f063513c3f929ca
aqucial <70082504+aqucial@users.noreply.github.com>
2020-09-14 22:23:18 +0900

    Add AtCoder Library

---
+++ library/template_atcoder.h
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+// using mint = modint1000000007;
+// using mint = modint998244353;
+// using mint = modint;
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr ll linf = 0x3f3f3f3f3f3f3f3fLL;
